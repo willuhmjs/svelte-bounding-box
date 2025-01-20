b1f44bd8d2698c9d68088de7540e64e366f5d26a
William Faircloth <willuhmjs@gmail.com>
2025-01-20 12:08:36 -0500

    better demo page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,18 +9,26 @@
     }
 
     let boxes: Box[] = [];
-
-    
 </script>
 
 <BoundingBox bind:boxes={boxes}>
-    <div style="width: 100%; height: 100%; background-color: lightgrey;">
+    <div class="content">
         <h1>Demo Content</h1>
         <p>Draw bounding boxes over this content.</p>
     </div>
 </BoundingBox>
 
-
-<pre>
+<pre style="background-color: #f0f0f0; padding: 10px; border: 1px solid #ccc;">
     {JSON.stringify(boxes, null, 2)}
-    </pre>
+</pre>
+<style>
+    .content {
+        padding: 20px;
+        border: 1px solid #ccc;
+        background-color: #f9f9f9;
+    }
+    
+    :global(body) {
+        font-family: sans-serif;
+    }
+</style>
